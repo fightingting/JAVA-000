ea1c0077d0aa7d348a01808f11642e6e052105a2
fightingting <18375337360@163.com>
2020-10-28 22:58:07 +0800

    第四课作业

---
+++ Week_02/第四课作业/HttpClientTest.java
@@ -0,0 +1,28 @@
+
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class HttpClientTest {
+    public static void main(String[] args) throws IOException {
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet("http://localhost:8801");
+        //发送请求
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        //判断状态码
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity entity = response.getEntity();
+            //使用工具类EntityUtils，从响应中取出实体表示的内容并转换成字符串
+            String string = EntityUtils.toString(entity, "utf-8");
+            System.out.println(string);
+        }
+        //关闭资源
+        response.close();
+        httpClient.close();
+    }
+}
